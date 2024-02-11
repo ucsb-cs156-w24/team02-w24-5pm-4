268097332dd4867c7689896b1d556886b1d88e41
anirudhiyer02 <46876585+anirudhiyer02@users.noreply.github.com>
2024-02-11 13:42:40 -0800

    Added fix for issues #40, #41, and #42

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -74,48 +74,48 @@ public class ArticlesController extends ApiController {
         return savedArticle;
     }
 
-    // @Operation(summary= "Get a single article")
-    // @PreAuthorize("hasRole('ROLE_USER')")
-    // @GetMapping("")
-    // public Articles getById(
-    //         @Parameter(name="id") @RequestParam Long id) {
-    //     Articles article = articlesRepository.findById(id)
-    //             .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+    @Operation(summary= "Get a single article")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public Articles getById(
+            @Parameter(name="id") @RequestParam Long id) {
+        Articles article = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
 
-    //     return article;
-    // }
+        return article;
+    }
 
-    // @Operation(summary= "Delete an Article")
-    // @PreAuthorize("hasRole('ROLE_ADMIN')")
-    // @DeleteMapping("")
-    // public Object deleteArticle(
-    //         @Parameter(name="id") @RequestParam Long id) {
-    //     Articles article = articlesRepository.findById(id)
-    //             .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+    @Operation(summary= "Delete an Article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteArticle(
+            @Parameter(name="id") @RequestParam Long id) {
+        Articles article = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
 
-    //     articlesRepository.delete(article);
-    //     return genericMessage("Article with id %s deleted".formatted(id));
-    // }
+        articlesRepository.delete(article);
+        return genericMessage("Article with id %s deleted".formatted(id));
+    }
 
-    // @Operation(summary= "Update a single date")
-    // @PreAuthorize("hasRole('ROLE_ADMIN')")
-    // @PutMapping("")
-    // public Articles updateArticle(
-    //         @Parameter(name="id") @RequestParam Long id,
-    //         @RequestBody @Valid Articles incoming) {
+    @Operation(summary= "Update a single date")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public Articles updateArticle(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid Articles incoming) {
 
-    //     Articles article = articlesRepository.findById(id)
-    //             .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+        Articles article = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
 
-    //             article.setTitle(incoming.getTitle());
-    //             article.setUrl(incoming.getUrl());
-    //             article.setExplanation(incoming.getExplanation());
-    //             article.setEmail(incoming.getEmail());
+                article.setTitle(incoming.getTitle());
+                article.setUrl(incoming.getUrl());
+                article.setExplanation(incoming.getExplanation());
+                article.setEmail(incoming.getEmail());
         
-    //             article.setDateAdded(incoming.getDateAdded());
+                article.setDateAdded(incoming.getDateAdded());
 
-    //     articlesRepository.save(article);
+        articlesRepository.save(article);
 
-    //     return article;
-    // }
+        return article;
+    }
 }
--- src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
@@ -96,7 +96,7 @@ public class ArticlesControllerTests extends ControllerTestCase{
                 assertEquals(expectedJson, responseString);
         }
 
-        // Tests for POST /api/ucsbdates/post...
+        // Tests for POST /api/articles/post...
 
         @Test
         public void logged_out_users_cannot_post() throws Exception {
@@ -140,5 +140,202 @@ public class ArticlesControllerTests extends ControllerTestCase{
                 String responseString = response.getResponse().getContentAsString();
                 assertEquals(expectedJson, responseString);
         }
+
+
+        // Tests for GET /api/articles?id=...
+        @Test
+        public void logged_out_users_cannot_get_by_id() throws Exception {
+                mockMvc.perform(get("/api/articles?id=7"))
+                                .andExpect(status().is(403)); // logged out users can't get by id
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_exists() throws Exception {
+
+                // arrange
+                LocalDateTime ldt = LocalDateTime.parse("2022-01-03T00:00:00");
+
+                Articles article = Articles.builder()
+                                .title("Best_places_to_eat")
+                                .url("https://www.tripadvisor.com/Restaurants-g32532-Isla_Vista_California.html")
+                                .explanation("An_article_for_the_best_places_to_eat")
+                                .email("anirudhiyer@ucsb.edu")
+                                .dateAdded(ldt)
+                                .build();
+
+                when(articlesRepository.findById(eq(7L))).thenReturn(Optional.of(article));
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/articles?id=7"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(articlesRepository, times(1)).findById(eq(7L));
+                String expectedJson = mapper.writeValueAsString(article);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
+
+                // arrange
+
+                when(articlesRepository.findById(eq(7L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/articles?id=7"))
+                                .andExpect(status().isNotFound()).andReturn();
+
+                // assert
+
+                verify(articlesRepository
+                , times(1)).findById(eq(7L));
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("EntityNotFoundException", json.get("type"));
+                assertEquals("Articles with id 7 not found", json.get("message"));
+        }
+
+
+
+        // Tests for PUT /api/articles?id=... 
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void admin_can_edit_an_existing_article() throws Exception {
+                // arrange
+
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+                LocalDateTime ldt2 = LocalDateTime.parse("2023-01-03T00:00:00");
+
+                Articles articlesOrig = Articles.builder()
+                                .title("Best_places_to_eat")
+                                .url("https://www.tripadvisor.com/Restaurants-g32532-Isla_Vista_California.html")
+                                .explanation("An_article_for_the_best_places_to_eat")
+                                .email("anirudhiyer@ucsb.edu")
+                                .dateAdded(ldt1)
+                                .build();
+
+
+                Articles articlesEdited = Articles.builder()
+                                .title("Best_places_to_travel")
+                                .url("https://travel.usnews.com/rankings/worlds-best-vacations/")
+                                .explanation("An_article_for_the_best_places_to_travel")
+                                .email("anirudhiyer1@ucsb.edu")
+                                .dateAdded(ldt2)
+                                .build();
+
+                String requestBody = mapper.writeValueAsString(articlesEdited);
+
+                when(articlesRepository.findById(eq(67L))).thenReturn(Optional.of(articlesOrig));
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                put("/api/articles?id=67")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .characterEncoding("utf-8")
+                                                .content(requestBody)
+                                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+                verify(articlesRepository, times(1)).findById(67L);
+                verify(articlesRepository, times(1)).save(articlesEdited); // should be saved with correct user
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(requestBody, responseString);
+        }
+
+        
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void admin_cannot_edit_articles_that_does_not_exist() throws Exception {
+                // arrange
+
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+                Articles articlesEdited = Articles.builder()
+                                .title("Best_places_to_eat")
+                                .url("https://www.tripadvisor.com/Restaurants-g32532-Isla_Vista_California.html")
+                                .explanation("An_article_for_the_best_places_to_eat")
+                                .email("anirudhiyer@ucsb.edu")
+                                .dateAdded(ldt1)
+                                .build();
+
+                String requestBody = mapper.writeValueAsString(articlesEdited);
+
+                when(articlesRepository.findById(eq(67L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                put("/api/articles?id=67")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .characterEncoding("utf-8")
+                                                .content(requestBody)
+                                                .with(csrf()))
+                                .andExpect(status().isNotFound()).andReturn();
+
+                // assert
+                verify(articlesRepository, times(1)).findById(67L);
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("Articles with id 67 not found", json.get("message"));
+
+        }
+
+        // Tests for DELETE /api/articles?id=... 
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void admin_can_delete_an_article() throws Exception {
+                // arrange
+
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+                Articles article1 = Articles.builder()
+                                .title("Best_places_to_eat")
+                                .url("https://www.tripadvisor.com/Restaurants-g32532-Isla_Vista_California.html")
+                                .explanation("An_article_for_the_best_places_to_eat")
+                                .email("anirudhiyer@ucsb.edu")
+                                .dateAdded(ldt1)
+                                .build();
+
+                when(articlesRepository.findById(eq(15L))).thenReturn(Optional.of(article1));
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                delete("/api/articles?id=15")
+                                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+                verify(articlesRepository, times(1)).findById(15L);
+                verify(articlesRepository, times(1)).delete(any());
+
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("Article with id 15 deleted", json.get("message"));
+        }
+        
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void admin_tries_to_delete_non_existant_articles_and_gets_right_error_message()
+                        throws Exception {
+                // arrange
+
+                when(articlesRepository.findById(eq(15L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                delete("/api/articles?id=15")
+                                                .with(csrf()))
+                                .andExpect(status().isNotFound()).andReturn();
+
+                // assert
+                verify(articlesRepository, times(1)).findById(15L);
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("Articles with id 15 not found", json.get("message"));
+        }
+
 
 }
